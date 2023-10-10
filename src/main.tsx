e88b70b4c3426ecc440d0dc9af7bee739ec02714
dgonzalez777 <1989.daniela@gmail.com>
2023-10-10 19:57:40 -0400

    Setup React-router, Redux, Styled Components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-
+// import { Provider } from 'react-redux'
+// import { setupStore } from './redux/store.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const NavbarWrapper = () => {
+  return (
+  <div>
+      {/* <Navbar/>
+      <Outlet/> */}
+  </div>
+  )
+};
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <NavbarWrapper/>,
+    children: [
+      {
+      path: "/",
+      element: (
+        <h1>Home</h1>
+      ),
+    }
+   ]
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     {/* <Provider store={setupStore()}> */}
+        {/* <App /> */}
+        <RouterProvider router={  router} />
+     {/* </Provider> */}
   </React.StrictMode>,
 )
